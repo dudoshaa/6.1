7ae4bcd59d9e45896cb3d870f0724c3a58d4810e
durdona <durdona>
2025-06-19 16:15:53 +0500

    .

--- js/app.js
+++ js/app.js
@@ -4,25 +4,26 @@ const user = document.getElementById("user");
 const deleteBtn = document.getElementById("delete__btn");
 const clearBtn = document.getElementById("clear__button");
 
-// refresh
+//refresh
+
 formButton.addEventListener("click", (e) => {
   e.preventDefault();
   reload();
   clearBtn.classList.remove("hidden");
 });
 
-// clear
+//clear
+
 clearBtn.addEventListener("click", (e) => {
   e.preventDefault();
   user.innerHTML = "";
   clearBtn.classList.add("hidden");
 });
 
-// search by name
+//search by name
 form["form__input"].addEventListener("input", () => {
-  const inputValue = form["form__input"].value.toLocaleLowerCase();
+  const inputValue = form["form__input"].value.toLowerCase();
   const name = document.querySelectorAll(".user__name");
-
   name.forEach((item) => {
     if (item.lastElementChild.textContent.toLowerCase().includes(inputValue)) {
       item.parentElement.classList.remove("hidden");
@@ -32,7 +33,7 @@ form["form__input"].addEventListener("input", () => {
   });
 });
 
-// get result and update UI
+//get result  and update UIS
 
 const updateUI = (data) => {
   user.innerHTML = "";
@@ -40,43 +41,42 @@ const updateUI = (data) => {
     const { gender, name, picture, location, dob } = item;
 
     user.innerHTML += `
-     <li class="user__item">
-              <button id="delete__btn" class="user__delete--btn">
-                <i class="fas fa-trash"></i>
-              </button>
-              <img
-                class="user__img"
-                alt="User photo"
-                src=${picture.large}
-                width="100"
-                height="100"
-              />
-              <div class="user__name">
-                <span class="material-symbols-outlined">badge</span>
-                <span>- ${name.title} ${name.first} ${name.last}</span>
-              </div>
-              <div class="user__year">
-                <span class="material-symbols-outlined">cake</span>
-                <span>- ${dob.age} years old.</span>
-              </div>
-              <div class="user__location">
-                <span class="material-symbols-outlined">person_pin_circle</span>
-                <span>-${location.city}, ${location.country}</span>
-              </div>
-              <div class="user__gender">
-                <span class="material-symbols-outlined">man</span>
-                <span>- ${gender}</span>
-              </div>
-            </li>`;
+    <li class="user__item">
+             <button id="delete__btn" class="user__delete--btn">
+               <i class="fas fa-trash"></i>
+             </button>
+             <img
+               class="user__img"
+               alt="User photo"
+               src=${picture.large}
+               width="100"
+               height="100"
+             />
+             <div class="user__name">
+               <span class="material-symbols-outlined">badge</span>
+               <span>-${name.title} ${name.first} ${name.last} </span>
+             </div>
+             <div class="user__year">
+               <span class="material-symbols-outlined">cake</span>
+               <span>- ${dob.age} years old.</span>
+             </div>
+             <div class="user__location">
+               <span class="material-symbols-outlined">person_pin_circle</span>
+               <span>-${location.city},${location.country}</span>
+             </div>
+             <div class="user__gender">
+               <span class="material-symbols-outlined">man</span>
+               <span>- ${gender}</span>
+             </div>
+           </li>`;
   });
 };
 
-// delete user
+//delete user
 document.addEventListener("click", (e) => {
   if (e.target.classList[0] === "user__delete--btn") {
     e.target.parentElement.remove();
   }
-
   if (!user.children.length) {
     clearBtn.classList.add("hidden");
   }
